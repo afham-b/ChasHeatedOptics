6f4d29551281afe09da984900edf3510e7dedc02
afham-b <afhambashir@yahoo.com>
2025-04-16 21:20:06 -0400

    found arduino reference usb voltage, tested with 100 ohm resistor to verify output was 0 Celcius

--- ArduinoCT325.py
+++ ArduinoCT325.py
@@ -8,15 +8,15 @@ board = pyfirmata.Arduino('COM10')  # Or '/dev/ttyUSB0' on Linux
 it = pyfirmata.util.Iterator(board)
 it.start()
 
-# Set up analog pin A0
+# Set up analog pin, A0 for our setup
 analog_input = board.get_pin('a:0:i')  # a = analog, 0 = pin A0, i = input
 
 # Read and convert voltage to temperature
 def read_temperature():
     reading = analog_input.read()
     if reading is not None:
-        voltage = reading * 5.0  # Arduino uses 5V reference
-        temp_c = voltage / 0.010  # 0.010 V/°C
+        voltage = reading * 5.03  # our Arduino uses 5.03V reference from usb power (this is measured using voltmeter)
+        temp_c = voltage / 0.010  # 0.010 V/°C (CT325)
         return temp_c
     return None
 
@@ -24,8 +24,11 @@ def read_temperature():
 print("Reading temperature from CT325...")
 while True:
     temp = read_temperature()
+
     if temp is not None:
-        print(f"Temperature: {temp:.2f} °C")
+        #print(f"Temperature: {temp:.2f} °C")
+        voltage = temp * 0.01  # 5V reference
+        print(f"Voltage: {voltage:.3f} V | Temperature: {temp:.2f} °C")
     else:
         print("Waiting for signal...")
     time.sleep(1)
